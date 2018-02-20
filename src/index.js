0f2425ccbd1a86342281d994ccf5e3e800c13500
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-20 20:08:42 +0300

    200/200 passing (133ms)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+    equation = equation.replace(/\s+/g, '');
+    var first = equation.match(/^[-]?\d+/g);
+    var second = equation.match(/x[^]2[+-]\d+[*]x/g)[0].match(/[+-]\d+/g);
+    var third = equation.match(/[+-]\d+$/g);
+    first = +first[0];
+    second = +second[0] / first;
+    third = +third[0] / first;
+    //first = 1;
+    var d = Math.pow(second,2) - 4 * third;
+    var x1 = (-second - Math.sqrt(d)) / 2;
+    var x2 = (-second + Math.sqrt(d)) / 2;
+    return [x1, x2];
 }
